57ce50976551c734a56729aa53802f7eabc75a5e
Miri Yoo <violinmiriu@gmail.com>
2022-10-25 12:29:44 +0200

    merge conflict solved for project/urls.py

--- backend/project/urls.py
+++ backend/project/urls.py
@@ -1,20 +1,13 @@
 from django.contrib import admin
-<<<<<<< HEAD
-from django.urls import path
+from django.urls import path, include
 from rest_framework_simplejwt import views
 
 urlpatterns = [
-    path("backend/admin/", admin.site.urls),
+    path("backend/api/admin/", admin.site.urls),
+    path("backend/api/restaurants/", include('restaurants.urls')),
 
     # JWT
     path('backend/token/', views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('backend/token/refresh/', views.TokenRefreshView.as_view(), name='token_refresh'),
     path('backend/token/verify/', views.TokenVerifyView.as_view(), name='token_verify'),
-=======
-from django.urls import path, include
-
-urlpatterns = [
-    path("backend/api/admin/", admin.site.urls),
-    path("backend/api/restaurants/", include('restaurants.urls'))
->>>>>>> fda5cf91575d0b6d7e28e40c691cdb6d65344fff
 ]
