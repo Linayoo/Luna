1a895166b8dd8708c7056a581556d3fdca49686d
Miri Yoo <violinmiriu@gmail.com>
2022-10-26 10:00:11 +0200

    flake 8 (lines, spaces fixed)

--- backend/users/views.py
+++ backend/users/views.py
@@ -3,12 +3,11 @@ import logging
 from rest_framework import filters
 from rest_framework.generics import RetrieveUpdateAPIView, ListAPIView, RetrieveAPIView, get_object_or_404
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
-
 from users.models import User
 from users.serializers import UserSerializer
 
 
-logger = logging.getLogger('django')
+# logger = logging.getLogger('django')  # This was implemented for django debugging
 
 
 # /api/me/
@@ -37,7 +36,7 @@ class ListAllUsers(ListAPIView):
 
 
 # /api/users/?search=<str:search_string>/
-# GET: Search for a user
+# GET: Search for a user (retrieve all the users that include search_string)
 class SearchUser(ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
