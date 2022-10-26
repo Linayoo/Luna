38be01973947a1b098867725355819c046800898
Miri Yoo <violinmiriu@gmail.com>
2022-10-26 20:51:19 +0200

    Restaurant/models.py: get_user_model() implemented

--- backend/restaurants/models.py
+++ backend/restaurants/models.py
@@ -1,8 +1,8 @@
-from datetime import datetime
-
+from django.contrib.auth import get_user_model
 from django.db import models
 
-from users.models import User
+
+User = get_user_model()
 
 
 class Restaurant(models.Model):
